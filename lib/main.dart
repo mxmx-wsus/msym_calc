7a07c90b91f8dc9116f731a7670b09ab8e58e573
mxm-wsus <mxmx.khaki@gmail.com>
2023-01-05 00:49:25 +0900

    コンフリクト修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:msym_calc/main_display.dart';
-import 'package:msym_calc/calc_keybord.dart';
-import 'text_data.dart';
+import 'package:msym_calc/component/my_app.dart';
 
 void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text(TextData.appTitle),
-            ),
-            body: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [MainDisplay(), CalcKeyboard()],
-            )));
-  }
-}
