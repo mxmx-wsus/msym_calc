24d500b9865db77edba5a74c285a2a6bf3cb0853
mxm-wsus <mxmx.khaki@gmail.com>
2023-01-12 22:18:10 +0900

    入力機能の実装

--- lib/component/calc_button.dart
+++ lib/component/calc_button.dart
@@ -1,23 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final DisplayControllerProvider = StateProvider((ref) => _displayController);
+
+String _displayController = '0';
+String get displayController => _displayController;
 
 class CalcButton extends StatelessWidget {
-  final String _keyWord;
+  final String keyWord;
   const CalcButton(
-    this._keyWord, {
+    this.keyWord, {
     Key? key,
   }) : super(key: key);
+
+  void addDisplayController() {
+    _displayController = keyWord;
+  }
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
       child: Center(
         child: Text(
-          _keyWord,
+          keyWord,
           style: TextStyle(fontSize: 46.0),
         ),
       ),
       onPressed: () {
-        //ボタンが押されたら「main_display.dart」に「_keyWord」を追加する。
+        //ボタンが押されたら「displayControllerProvider」に「_keyWord」を追加する。
+        addDisplayController();
       },
     );
   }
--- lib/component/main_display.dart
+++ lib/component/main_display.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:msym_calc/component/calc_button.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MainDisplay extends StatefulWidget {
-  _MainDisplayState createState() => _MainDisplayState();
-}
-
-class _MainDisplayState extends State<MainDisplay> {
-  String _expression = '1+1';
+class MainDisplay extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final displayControllerProvider = ref.watch(DisplayControllerProvider);
     return Expanded(
         flex: 1,
         child: Container(
           alignment: Alignment.centerRight,
           child: Text(
-            _expression,
+            displayControllerProvider.toString(),
             style: TextStyle(
               fontSize: 64.0,
             ),
